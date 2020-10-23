670520a71624a1bfe1574a9d281318ca1d3a4d2e
Khokhlova <koklova@alice.it>
2020-10-23 22:37:01 +0200

    Second commit

--- script.js
+++ script.js
@@ -3,37 +3,32 @@
     let num = Math.floor(Math.random()*101);
     return num;
 }
-stateANumber();
-let secretnumber = stateANumber();
-
 
 function guessNumber() {
-
+    let secretnumber = stateANumber();
     let number;
 
     function enterYourNumber() {
-        number = prompt('Угадай число от 1 до 100');
+        number = prompt('Угадай число от 1 до 100');  
+        checkNumber();                                                  
+    }   
+
+    function checkNumber() {         
         if (isNaN(number)) {
-            alert('Введи число!');            
-            enterYourNumber();
+            alert('Введи число!');
+            enterYourNumber();            
+        } else if (number > secretnumber) {
+            alert('Загаданное число меньше');
+            enterYourNumber();                        
+        } else if (number < secretnumber) {
+            alert('Загаданное число больше');
+            enterYourNumber();                    
+        } else if (parseFloat(number) === secretnumber) {
+            alert('Поздравляю, вы угадали!!!');
         } else if (number === null) {
-            alert('Game over');
-            return;                                                                                                        
-        }  
-        return number;                    
-    }        
-    
-    enterYourNumber();  
-       
-    if (number > secretnumber) {
-        alert('Загаданное число меньше');
-        guessNumber();             
-    } else if (number < secretnumber) {
-        alert('Загаданное число больше');
-        guessNumber();        
-    } else if (parseFloat(number) === secretnumber) {
-        alert('Поздравляю, вы угадали!!!');
-    }  
+            alert('Game over');                                                                                                            
+        }         
+    }
 }
 
-guessNumber(); 
+guessNumber();
