1dc11f89269e427b74f9175e4dce732dc57b50fa
Khokhlova <koklova@alice.it>
2020-10-24 13:50:17 +0200

    Third the right one commit

--- script.js
+++ script.js
@@ -8,27 +8,30 @@ function guessNumber() {
     let secretnumber = stateANumber();
     let number;
 
-    function enterYourNumber() {
-        number = prompt('Угадай число от 1 до 100');  
-        checkNumber();                                                  
-    }   
-
-    function checkNumber() {         
-        if (isNaN(number)) {
-            alert('Введи число!');
-            enterYourNumber();            
-        } else if (number > secretnumber) {
+    function checkNumber() {                    
+        if (number > secretnumber) {
             alert('Загаданное число меньше');
             enterYourNumber();                        
         } else if (number < secretnumber) {
             alert('Загаданное число больше');
             enterYourNumber();                    
         } else if (parseFloat(number) === secretnumber) {
-            alert('Поздравляю, вы угадали!!!');
-        } else if (number === null) {
-            alert('Game over');                                                                                                            
+            alert('Поздравляю, вы угадали!!!');                                                                                                                    
         }         
     }
+
+    function enterYourNumber() {
+        number = prompt('Угадай число от 1 до 100');
+        if (number === null) {
+            alert('Game over'); 
+        } else if (isNaN(parseFloat(number)) || number === '') {
+            alert('Введи число!');
+            enterYourNumber();                        
+        } else {
+            checkNumber();
+        }                                                     
+    }
+    enterYourNumber();
 }
 
 guessNumber();
